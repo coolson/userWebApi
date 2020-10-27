10025cef648c4fda5661c99ac7da560ca5476d14
kulivers <kulivers@mail.ru>
2020-10-27 18:55:15 +0300

    all fixed

--- userWebApi/ClientApp/src/components/Home.tsx
+++ userWebApi/ClientApp/src/components/Home.tsx
@@ -9,7 +9,8 @@ enum Education {
 class Home extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { name: '', isMale: true, education: Education.High, hasCar: false, message: '' };
+        this.state = { name: '', isMale: true, education: Education.High, hasCar: false, messages: [] };
+        this.handleHasCar = this.handleHasCar.bind(this);
     }
 
     handleChange = (event) => {
@@ -20,18 +21,27 @@ class Home extends React.Component {
         this.setState({ [event.target.name]: event.target.value !== "false" });
     }
 
+    handleHasCar = (event) => {
+        let isChecked = event.target.checked;
+        this.setState({ hasCar: isChecked });
+    }
+
+    handleReset = (event) => {
+        this.setState( { name: '', isMale: true, education: Education.High, hasCar: false, messages: [] });
+
+    }
+    
     handleSubmit = (event) => {
         var that = this;
         fetch('api/UserRegister', {
             method: 'POST',
-            headers: {'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             // We convert the React state to JSON and send it as the POST body
             body: JSON.stringify(this.state)
         }).then(function (response) {
             console.log(response);
-            response.json().then(function (json)
-            {
-                that.setState({ message: !response.ok ? `status: ${response.statusText}` : json.messages.join('<br>') });    
+            response.json().then(function (json) {
+                that.setState({ messages: !response.ok ? [`status: ${response.statusText}`] : json.messages });
             });
         });
 
@@ -41,14 +51,14 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.handleSubmit} onReset = {this.handleReset}>
                     <h1> Заполните анкету:<br /></h1>
                     <h2> Введите фамилию, имя:<br /></h2>
                     <input type='text' name='name' value={this.state.name} onChange={this.handleChange} /><br />
                     <h3> Укажите пол:<br /></h3>
                     <input type="RADIO" checked={this.state.isMale} name="isMale" value="true" onChange={this.handleBoolChange} /> Мужской<br />
                     <input type="RADIO" checked={!this.state.isMale} name="isMale" value="false" onChange={this.handleBoolChange}/> Женский
-            <h3>  Укажите образование:<br /></h3>
+                    <h3>  Укажите образование:<br /></h3>
                     <select name="education"
                         value={this.state.education}
                         onChange={this.handleChange}>
@@ -57,7 +67,7 @@ class Home extends React.Component {
                         <option value={Education.Middle}>Среднее полное</option>
                     </select>
                     <h3>  Наличие авто:<br /></h3>
-                    <input type="CHECKBOX" name="hasCar" checked={this.state.hasCar} value={this.state.hasCar ? "true" : "false"} onChange={this.handleBoolChange} />
+                    <input type="checkbox" onClick={this.handleHasCar}  name = "hasCar"/>
 
                     <div><p>
                         <input type="submit" value="Отправить" />
@@ -65,7 +75,10 @@ class Home extends React.Component {
                     </p></div>
                 </form>
                 <div>
-                    {this.state.message}
+                    <ul>
+                        {this.state.messages.map(x => (<li>{x}</li>))}
+                    </ul>
+
                 </div>
 
             </div>
